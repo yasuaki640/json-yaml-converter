0b108c5bb6d126c3d3a826d23ffb0c16bc72d2b8
yasuaki640 <yasuaki640@gmail.com>
2021-03-10 08:37:53 +0900

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -11,20 +9,13 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         System.out.println("*** The JSON <-> YAML converter ***");
-
         System.out.println("Enter a path to target file -> ");
         var sc = new Scanner(System.in);
-        sc.next();
     }
 
     public static List<String> getText(String pathToFile) throws IOException {
-        var path = Paths.get(pathToFile);
-        try {
-            return Files.lines(path, StandardCharsets.UTF_8)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            System.out.println("File doesnt exists.");
-            throw e;
-        }
+        return Files
+                .lines(Paths.get(pathToFile), StandardCharsets.UTF_8)
+                .collect(Collectors.toList());
     }
 }
